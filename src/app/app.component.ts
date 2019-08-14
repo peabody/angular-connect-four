e1f4703c8f27b4c3ab94306fabc43364fd420244
Sam Peterson <petersons@calaces.org>
2019-08-14 01:32:56 -0700

    Fixed bugs, made more presentable for phones.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,33 +26,35 @@ export class AppComponent {
   gameOver = false
   winMsg = ''
   winningCoordinates: number[][] = []
+  stats = { Red: 0, Yellow: 0}
+  autoplay = false
+  interval: any
 
   onClick(cell: any) {
     if (!this.gameOver) {
       let i = cell.parentElement.rowIndex
       let j = cell.cellIndex
       if (this.board[i][j] === null) {
-        this.board[i][j] = this.switchColor()
-        this.movePiece()
-        this.checkWin()
+        this.board[i][j] = this.switchColor();
+        [i,j] = this.movePiece(i,j)
+        this.checkWin(i,j)
       }
     }
   }
 
-  movePiece() {
-    for (let i=0; i<this.board.length; i++) {
-      for (let j=0; j<this.board[i].length; j++) {
-        try {
-          if (this.board[i+1][j] === null) {
-            this.board[i+1][j] = this.board[i][j]
-            this.board[i][j] = null
-          }  
-        } catch(err) {
-          if (!(err instanceof TypeError))
-            console.error(err)
-        }
+  movePiece(i: number, j: number) {
+    let moveCheck = i
+    try {
+      while(this.board[moveCheck+1][j] === null) {
+        this.board[moveCheck+1][j] = this.board[moveCheck][j]
+        this.board[moveCheck][j] = null
+        moveCheck++
       }
+    } catch(err) {
+      if (!(err instanceof TypeError))
+        console.error(err)
     }
+    return [moveCheck,j]
   }
 
   resetBoard() {
@@ -60,18 +62,23 @@ export class AppComponent {
     this.gameOver = false
     this.winMsg = ''
     this.winningCoordinates = []
+    this.autoplay = false
+    if (this.interval) {
+      clearInterval(this.interval)
+      this.interval = null
+    }
   }
 
-  checkWin() {
-    for (let i=0; i<this.board.length; i++) {
-      for (let j=0; j<this.board[i].length; j++) {
-        let winningColor: any = this.findFour(i,j)
-        if (winningColor) {
-          this.gameOver = true
-          this.winMsg = `${winningColor} wins!`
-          this.colorWinningCoordinates()
-          return
-        }
+  checkWin(i: number, j: number) {
+    let winningColor: any = this.findFour(i,j)
+    if (winningColor) {
+      this.gameOver = true
+      this.winMsg = `${winningColor} wins!`
+      this.stats[winningColor]++
+      this.colorWinningCoordinates()
+      if (this.interval) {
+        clearInterval(this.interval)
+        this.interval = null
       }
     }
   }
@@ -82,23 +89,6 @@ export class AppComponent {
 
     // check up down
     let upDownCount = 0
-
-    // up loop
-    for (let i_check=-1; i_check<-4; i_check--) {
-      try {
-        if (this.board[i + i_check][j] === checkColor) {
-          this.winningCoordinates.push([i + i_check, j])
-          upDownCount++
-        } else {
-          break;
-        }
-      } catch (err) {
-        if (!(err instanceof TypeError)) {
-          console.error(err)
-        }
-        break;
-      }
-    }
 
     // down loop
     for (let i_check=1; i_check<4; i_check++) {
@@ -122,16 +112,15 @@ export class AppComponent {
       return checkColor;
     }
 
-
     // check right left
     let rightLeftCount = 0
     this.winningCoordinates = []
 
-    // left loop
-    for (let j_check=-1; j_check<-4; j_check--) {
+    // right loop
+    for (let j_check=1; j_check<4; j_check++) {
       try {
         if (this.board[i][j + j_check] === checkColor) {
-          this.winningCoordinates.push([i, j + j_check])                    
+          this.winningCoordinates.push([i, j + j_check])                              
           rightLeftCount++
         } else {
           break;
@@ -143,12 +132,12 @@ export class AppComponent {
         break;
       }
     }
-    
-    // right loop
-    for (let j_check=1; j_check<4; j_check++) {
+
+    // left loop
+    for (let j_check=-1; j_check>-4; j_check--) {
       try {
         if (this.board[i][j + j_check] === checkColor) {
-          this.winningCoordinates.push([i, j + j_check])                              
+          this.winningCoordinates.push([i, j + j_check])                    
           rightLeftCount++
         } else {
           break;
@@ -171,7 +160,7 @@ export class AppComponent {
     this.winningCoordinates = []
 
     // up left loop
-    for (let ij_check=-1; ij_check<-4; ij_check--) {
+    for (let ij_check=-1; ij_check>-4; ij_check--) {
       try {
         if (this.board[i + ij_check][j + ij_check] === checkColor) {
           this.winningCoordinates.push([i + ij_check, j + ij_check])                              
@@ -267,17 +256,18 @@ export class AppComponent {
   move4me() {
     if (!this.gameOver) {
       let tryMove = true
+      let i,j;
       while (tryMove) {
-        let i = 0
-        let j = Math.floor(Math.random() * 6)
+        i = 0
+        j = Math.floor(Math.random() * 7)
         tryMove = this.board[i][j] !== null
         if (!tryMove) {
           this.board[i][j] = this.getColor()
         }
       }
-      this.movePiece()
+      [i,j] = this.movePiece(i,j)
       this.switchColor()
-      this.checkWin()
+      this.checkWin(i,j)
     }
   }
 
@@ -285,6 +275,19 @@ export class AppComponent {
     for (let val=0; val<this.winningCoordinates.length; val++) {
       let coord = this.winningCoordinates[val]
       this.board[coord[0]][coord[1]] = "Green"
+    }
+  }
+
+  toggleAutoPlay() {
+    this.autoplay = !this.autoplay
+    if (this.interval) {
+      clearInterval(this.interval)
+      this.interval = null
+    }
+
+    if (this.autoplay) {
+      this.move4me()
+      this.interval = setInterval(this.move4me.bind(this), 500)
     }
   }
 }
